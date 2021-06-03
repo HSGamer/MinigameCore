aec92f26642a7502c19dece547bfbbe59c4cb30f
Huynh Tien <huynhqtienvtag@gmail.com>
2021-06-03 12:56:22 +0700

    Java doesn't like poor generic

--- MinigameCore-base/src/main/java/me/hsgamer/minigamecore/base/ArenaManager.java
+++ MinigameCore-base/src/main/java/me/hsgamer/minigamecore/base/ArenaManager.java
@@ -2,10 +2,10 @@ package me.hsgamer.minigamecore.base;
 
 import java.util.*;
 
-public abstract class ArenaManager<A extends Arena> implements Initializer {
+public abstract class ArenaManager implements Initializer {
     private final Map<Class<? extends GameState>, GameState> gameStateMap = new IdentityHashMap<>();
     private final Map<Class<? extends Feature>, Feature> featureMap = new IdentityHashMap<>();
-    private final List<A> arenaList = new LinkedList<>();
+    private final List<Arena> arenaList = new LinkedList<>();
 
     @Override
     public void init() {
@@ -34,7 +34,7 @@ public abstract class ArenaManager<A extends Arena> implements Initializer {
 
     public abstract List<Feature> loadFeatures();
 
-    public abstract List<A> loadArenas();
+    public abstract List<Arena> loadArenas();
 
     public <T extends GameState> T getGameState(Class<T> gameStateClass) {
         GameState gameState = gameStateMap.get(gameStateClass);
@@ -46,11 +46,11 @@ public abstract class ArenaManager<A extends Arena> implements Initializer {
         return featureClass.isInstance(feature) ? featureClass.cast(feature) : null;
     }
 
-    public Optional<A> getArenaByName(String name) {
+    public Optional<Arena> getArenaByName(String name) {
         return arenaList.parallelStream().filter(arena -> arena.getName().equals(name)).findFirst();
     }
 
-    public List<A> getAllArenas() {
+    public List<Arena> getAllArenas() {
         return Collections.unmodifiableList(arenaList);
     }
 }
