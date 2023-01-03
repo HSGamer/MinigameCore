c3fe0137322bce994841d73dffdd3afa524c1bb2
HSGamer <huynhqtienvtag@gmail.com>
2023-01-03 12:30:45 +0700

    remove getName from Feature

--- minigamecore-base/src/main/java/me/hsgamer/minigamecore/base/Feature.java
+++ minigamecore-base/src/main/java/me/hsgamer/minigamecore/base/Feature.java
@@ -4,12 +4,4 @@ package me.hsgamer.minigamecore.base;
  * The feature of the arena
  */
 public interface Feature extends Initializer {
-    /**
-     * Get the name of the feature
-     *
-     * @return the name
-     */
-    default String getName() {
-        return getClass().getSimpleName();
-    }
 }
