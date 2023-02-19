3d16c518f0903b79df23103662ab194390029ce4
HSGamer <huynhqtienvtag@gmail.com>
2023-02-19 16:03:06 +0700

    register feature with super classes

--- minigamecore-base/src/main/java/me/hsgamer/minigamecore/base/ArenaUnit.java
+++ minigamecore-base/src/main/java/me/hsgamer/minigamecore/base/ArenaUnit.java
@@ -1,8 +1,6 @@
 package me.hsgamer.minigamecore.base;
 
-import java.util.IdentityHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * An internal class for the arena {@link Feature} and {@link GameState}
@@ -11,13 +9,40 @@ class ArenaUnit implements Initializer {
     private final Map<Class<? extends GameState>, GameState> gameStateMap = new IdentityHashMap<>();
     private final Map<Class<? extends Feature>, Feature> featureMap = new IdentityHashMap<>();
 
+    private static <T> Set<Class<? extends T>> getSuperClasses(Class<T> baseClass, Class<? extends T> childClass) {
+        Set<Class<? extends T>> classSet = new HashSet<>();
+        classSet.add(childClass);
+
+        Queue<Class<? extends T>> classQueue = new LinkedList<>();
+        classQueue.add(childClass);
+        while (true) {
+            Class<? extends T> currentClass = classQueue.poll();
+            if (currentClass == null) {
+                break;
+            }
+
+            List<Class<?>> superClasses = new ArrayList<>(Arrays.asList(currentClass.getInterfaces()));
+            Optional.ofNullable(currentClass.getSuperclass()).ifPresent(superClasses::add);
+
+            for (Class<?> superClassOrInterface : superClasses) {
+                if (baseClass.isAssignableFrom(superClassOrInterface)) {
+                    Class<? extends T> superClassOrInterfaceSubClass = superClassOrInterface.asSubclass(baseClass);
+                    if (classSet.add(superClassOrInterfaceSubClass)) {
+                        classQueue.add(superClassOrInterfaceSubClass);
+                    }
+                }
+            }
+        }
+        return classSet;
+    }
+
     /**
      * Load the game states
      *
      * @param units the units
      */
     void loadGameStates(List<Unit<GameState>> units) {
-        units.forEach(unit -> gameStateMap.put(unit.clazz, unit.instance));
+        units.forEach(unit -> getSuperClasses(GameState.class, unit.clazz).forEach(clazz -> gameStateMap.put(clazz, unit.instance)));
     }
 
     /**
@@ -26,7 +51,7 @@ class ArenaUnit implements Initializer {
      * @param units the units
      */
     void loadFeatures(List<Unit<Feature>> units) {
-        units.forEach(unit -> featureMap.put(unit.clazz, unit.instance));
+        units.forEach(unit -> getSuperClasses(Feature.class, unit.clazz).forEach(clazz -> featureMap.put(clazz, unit.instance)));
     }
 
     @Override
