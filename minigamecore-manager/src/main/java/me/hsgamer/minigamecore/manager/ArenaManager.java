b47a87cae62fdb1c8a47a5b7f63132b477dcc916
HSGamer <huynhqtienvtag@gmail.com>
2023-07-24 11:27:56 +0700

    add createArena in ArenaManager

--- minigamecore-manager/src/main/java/me/hsgamer/minigamecore/manager/ArenaManager.java
+++ minigamecore-manager/src/main/java/me/hsgamer/minigamecore/manager/ArenaManager.java
@@ -3,7 +3,9 @@ package me.hsgamer.minigamecore.manager;
 import me.hsgamer.minigamecore.base.Arena;
 import me.hsgamer.minigamecore.base.FeatureUnit;
 
+import java.lang.reflect.Constructor;
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 /**
@@ -91,5 +93,44 @@ public abstract class ArenaManager extends FeatureUnit {
             arena.clear();
         });
         arenaList.clear();
+    }
+
+    /**
+     * Create an arena
+     *
+     * @param name             the name of the arena
+     * @param arenaClass       the class of the arena
+     * @param onCreateConsumer the consumer that will be called when the arena is created
+     * @param <T>              the type of the arena
+     * @return the arena or empty if it cannot be created
+     */
+    public <T extends Arena> Optional<T> createArena(String name, Class<T> arenaClass, Consumer<T> onCreateConsumer) {
+        T arena = null;
+
+        try {
+            Constructor<T> constructor = arenaClass.getConstructor(String.class, FeatureUnit.class);
+            arena = constructor.newInstance(name, this);
+        } catch (Exception ignored) {
+            // IGNORED
+        }
+
+        if (arena == null) {
+            try {
+                Constructor<T> constructor = arenaClass.getConstructor(String.class);
+                arena = constructor.newInstance(name);
+            } catch (Exception ignored) {
+                // IGNORED
+            }
+        }
+
+        if (arena == null) return Optional.empty();
+
+        onCreateConsumer.accept(arena);
+
+        if (addArena(arena)) {
+            return Optional.of(arena);
+        } else {
+            return Optional.empty();
+        }
     }
 }
