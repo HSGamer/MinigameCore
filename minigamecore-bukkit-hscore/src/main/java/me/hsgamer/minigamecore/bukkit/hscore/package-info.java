fec813a0df210912d608e3473f34e56430eceb99
HSGamer <huynhqtienvtag@gmail.com>
2024-03-21 00:22:58 +0700

    remove bukkit-hscore

--- minigamecore-bukkit-hscore/src/main/java/me/hsgamer/minigamecore/bukkit/hscore/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains the implementation of {@link me.hsgamer.minigamecore.bukkit.BukkitArena} that uses HSCore's {@link me.hsgamer.hscore.bukkit.scheduler.Scheduler}
- */
-package me.hsgamer.minigamecore.bukkit.hscore;
